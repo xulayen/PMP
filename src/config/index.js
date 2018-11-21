883414090e16fe399458089ff6e807b024734258
棹孤舟 <xulayen@163.com>
2018-11-21 19:48:28 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,11 @@
 /**gitalk评论系统  */
 module.exports = {
     gitalk: {
-        clientID: '468914a2f6df58fa9486',
-        clientSecret: 'a395496e467f9c0677c098a6c092cf8932bf1d39',
-        repo: 'PMP',
-        owner: 'xulayen',
-        admin: 'xulayen',
+        clientID: '',
+        clientSecret: '',
+        repo: '',
+        owner: '',
+        admin: '',
         id: window.location.pathname,      // Ensure uniqueness and length less than 50
         distractionFreeMode: false  // Facebook-like distraction free mode
     }
